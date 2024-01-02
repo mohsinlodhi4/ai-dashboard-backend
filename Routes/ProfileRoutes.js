f3aec507a953438abd737a6f169a3af39e7c1709
Mohsin Khan Lodhi <mohsin.lodhi@idenfo.com>
2024-01-03 01:09:14 +0500

    profile routes

---
+++ Routes/ProfileRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const ProfileController = require('../controllers/profileController');
+const {body, param} = require('express-validator');
+const validationResultMiddleware = require('../middlewares/validationResultMiddleware');  
+
+/** uses auth middleware **/
+
+router.get('/', ProfileController.getCompanyProfile);
+
+
+// step 1
+router.post('/submit-web-url', 
+body('webUrl').notEmpty().withMessage("webUrl field is required"), 
+validationResultMiddleware,
+ProfileController.submitWebUrlAndScrapeData )
+
+// step 2, 3
+router.post('/save', ProfileController.submitCompanyDetails)
+
+
+module.exports = router;
--- Routes/index.js
+++ Routes/index.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 const authMiddleware = require('../middlewares/auth');
 const authRouter = require('./AuthRoutes');
 const chatRouter = require('./ChatRoutes');
+const profileRouter = require('./ProfileRoutes');
 
 router.use('/auth',authRouter);
 router.use('/chat', authMiddleware, chatRouter);
+router.use('/profile', authMiddleware, profileRouter);
 
 module.exports = router;
