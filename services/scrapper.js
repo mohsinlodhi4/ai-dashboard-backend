69ea9561b3338b4b0fb3e7e6f68a7966a5d7c3ee
Mohsin Khan Lodhi <mohsin.lodhi@idenfo.com>
2024-01-04 12:52:21 +0500

    puppeteer fix

--- services/scrapper.js
+++ services/scrapper.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeWebsiteAndExtractData(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   // Extract data from the main page
